a03a2ea6aba93485372b396b3bf2230505ae9b1e
mackliu <macklun@gmail.com>
2023-11-13 13:23:21 +0800

    使用header來導向檔案

--- add_user.php
+++ add_user.php
@@ -6,3 +6,5 @@ $sql="insert into `users`(`acc`,`pw`,`name`,`email`,`address`)
                    values('{$_POST['acc']}','{$_POST['pw']}','{$_POST['name']}','{$_POST['email']}','{$_POST['address']}')";
 
 $pdo->exec($sql);
+
+header("Location:index.php");
