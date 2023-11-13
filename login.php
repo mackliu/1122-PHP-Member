1648e9f191aca51a88c3b73232609d0187eb06df
mackliu <macklun@gmail.com>
2023-11-13 15:58:43 +0800

    簡化的登入判斷方式

--- login.php
+++ login.php
@@ -6,10 +6,14 @@ $pw=$_POST['pw'];
 $dsn="mysql:host=localhost;charset=utf8;dbname=member";
 $pdo=new PDO($dsn,'root','');
 
-$sql="select * from users where `acc`='$acc' && `pw`='$pw'";
-$user=$pdo->query($sql)->fetch();
+//$sql="select * from users where `acc`='$acc' && `pw`='$pw'";
+$sql="select count(*) from users where `acc`='$acc' && `pw`='$pw'";
+//$user=$pdo->query($sql)->fetch();
+$user=$pdo->query($sql)->fetchColumn();
+//print_r($user);
 
-if($user['acc']==$acc && $user['pw']==$pw){
+//if($user['acc']==$acc && $user['pw']==$pw){
+if($user){
     $_SESSION['user']=$acc;
     header("location:index.php");
 }else{
